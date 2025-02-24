19a72741b31851b66b9c47667c072e9947b3062f
d3ller <corentin.nelhomme@gmail.com>
2025-02-24 10:57:48 +0100

    ♻️refactor: Add subscription data in user test

--- src/Tests/Entity/UserTest.php
+++ src/Tests/Entity/UserTest.php
@@ -2,6 +2,7 @@
 // tests/Entity/UserTest.php
 namespace App\Tests\Entity;
 
+use App\Entity\Subscription;
 use App\Entity\User;
 use PHPUnit\Framework\TestCase;
 
@@ -9,6 +10,18 @@ class UserTest extends TestCase
 {
     public function testGetterAndSetter()
     {
+        $subscription = new Subscription();
+
+        // Définition de données de test
+        $name = 'Test';
+        $description = 'Test description';
+        $price = 100;
+        $maxUsage = 10;
+        $subscription->setName($name);
+        $subscription->setDescription($description);
+        $subscription->setPrice($price);
+        $subscription->setMaxPdf($maxUsage);
+
         // Création d'une instance de l'entité User
         $user = new User();
 
@@ -24,6 +37,7 @@ class UserTest extends TestCase
         $user->setFirstname($name[1]);
         $user->setPassword('password');
         $user->setRoles($role);
+        $user->setSubscription($subscription);
 
         // Vérification des getters
         $this->assertEquals($email, $user->getEmail());
