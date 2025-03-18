d0753c8c3bfb8c8a31fe77b5500f63946929bb41
d3ller <corentin.nelhomme@gmail.com>
2025-03-18 11:41:23 +0100

    🚀 feat: added dashboard

---
+++ src/Controller/DashboardController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\FileRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+
+#[Route('/dashboard')]
+final class DashboardController extends AbstractController
+{
+    #[Route('/', name: 'app_dashboard')]
+    public function index(FileRepository $fileRepository): Response
+    {
+        $user = $this->getUser();
+        $pdfCount = $fileRepository->countUserFilesThisMonth($user->getId());
+
+        return $this->render('dashboard/index.html.twig', [
+            'controller_name' => 'DashboardController',
+            'user' => $user,
+            'pdfCount' => $pdfCount,
+        ]);
+    }
+
+    #[Route('/history', name: 'app_dashboard_historique')]
+    public function historique(): Response
+    {
+        return $this->render('dashboard/history.html.twig', [
+            'controller_name' => 'DashboardController',
+        ]);
+    }
+}
